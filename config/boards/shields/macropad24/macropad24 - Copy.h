6d22e5ba93177674e48d64d6e3748f88291fb992
StaticCrowbar <rbradyca@gmail.com>
2025-10-27 17:39:49 -0700

    Corrected pin mapping and updated keymap

--- config/boards/shields/macropad24/macropad24 - Copy.h
+++ config/boards/shields/macropad24/macropad24 - Copy.h
@@ -10,12 +10,12 @@
             label = "Default";
             sensor-bindings = <&inc_dec_kp K_VOL_UP K_VOL_DN>;
             bindings = <
-                &kp A    &kp G         &kp K_MUTE
-                &kp B    &kp KP_NLCK   &kp SLASH    &kp ASTRK   &kp MINUS
-                &kp C    &kp N7        &kp N8       &kp N9
-                &kp D    &kp N4        &kp N5       &kp N6      &kp PLUS
-                &kp E    &kp N1        &kp N2       &kp N3
-                &kp F    &kp LBKT      &kp RBKT     &kp DOT     &mo RET
+                         &kp A         &kp G                             &kp K_MUTE
+                &kp B    &kp KP_NLCK   &kp KP_DIVIDE   &kp KP_ASTERISK   &kp KP_MINUS
+                &kp C    &kp KP_N7     &kp KP_N8       &kp KP_N9
+                &kp D    &kp KP_N4     &kp KP_N5       &kp KP_N6         &kp KP_PLUS
+                &kp E    &kp KP_N1     &kp KP_N2       &kp KP_N3
+                &kp F                  &kp KP_N0       &kp KP_DOT        &mo KP_ENTER
             >;
         };
 
@@ -23,12 +23,12 @@
             label = "Function";8
             sensor-bindings = <&inc_dec_kp C_BRI_UP C_BRI_DN>;
             bindings = <
-                &bt BT_SEL 0    &bt BT_SEL 1    &bt BT_CLR
-                &kp F1          &kp F2          &kp F3          &kp F4          &kp F5
-                &kp F6          &kp F7          &kp F8          &kp F9
-                &kp F10         &kp F11         &kp F12         &trans          &trans
-                &trans          &trans          &trans          &trans
-                &trans          &trans          &trans          &trans          &trans
+				              &trans     &trans                &trans
+                &bt BT_SEL 0  &trans     &trans       &trans   &trans
+                &bt BT_SEL 1  &trans     &trans       &trans
+                &bt BT_CLR    &trans     &trans       &trans   &trans
+                &trans        &trans     &trans       &trans
+                &bootloader              &trans       &trans   &trans
             >;
         };
     };
